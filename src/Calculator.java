9e84798e1e61b4d4b746380eb8cf752e470888e9
Linda <lindasolberg5@hotmail.com>
2020-09-20 23:28:03 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    //a basic calculator that + - * / and modulo
+    public Calculator() {
+
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int devide(int a, int b) {
+        return a / b;
+    }
+
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(5,7));
+
+        System.out.println(myCalculator.subtract(45,11));
+
+        System.out.println(myCalculator.multiply(12,34));
+
+        System.out.println(myCalculator.devide(92,4));
+
+        System.out.println(myCalculator.modulo(10,1));
+    }
+}
